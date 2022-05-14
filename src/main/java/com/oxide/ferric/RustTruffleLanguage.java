d17ec2d0aa46e92a52843d5d558a4e8b860aa0ac
psvri <psvrishabh@gmail.com>
2022-05-14 20:15:17 +0530

    Minor edits

--- src/main/java/com/oxide/ferric/RustTruffleLanguage.java
+++ src/main/java/com/oxide/ferric/RustTruffleLanguage.java
@@ -7,8 +7,18 @@ import com.oracle.truffle.api.RootCallTarget;
 import com.oracle.truffle.api.TruffleLanguage;
 import com.oracle.truffle.api.frame.FrameDescriptor;
 
-@TruffleLanguage.Registration(id = "rs", name = "Rust")
+@TruffleLanguage.Registration(id = RustTruffleLanguage.ID,
+        name = RustTruffleLanguage.NAME,
+        defaultMimeType = RustTruffleLanguage.MIME_TYPE,
+        characterMimeTypes = RustTruffleLanguage.MIME_TYPE,
+        contextPolicy = TruffleLanguage.ContextPolicy.EXCLUSIVE,
+        interactive = false)
 public class RustTruffleLanguage extends TruffleLanguage<Void> {
+
+    public static final String ID = "rs";
+    public static final String MIME_TYPE = "application/rs";
+    public static final String NAME = "rust";
+    public static final String RUST_SOURCE_NAME_SUFFIX = ".rs";
 
     @Override
     protected RootCallTarget parse(ParsingRequest request) throws Exception {
